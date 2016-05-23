65c5088c073a27f7ac9f78661a948c658ed8adfe
zawisz <zawistowski.zawisz@gmail.com>
2016-05-23 17:35:39 +0200

    added tests for Register&Resolve operations

---
+++ tests/Container.Tests/yeti_container_tests.cs
@@ -0,0 +1,29 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Container.Tests
+{
+    public class yeti_container_tests
+    {
+        [Fact]
+        public void resolves_registerd_components()
+        {
+            var container = new YeTiContainer();
+            container.Register<ITestInterface, TestImplementation>();
+
+            var resolved_object = container.Resolve<ITestInterface>();
+
+            resolved_object.ShouldBeOfType<TestImplementation>();
+
+        }
+
+        public interface ITestInterface
+        {
+             
+        }
+
+        private class TestImplementation : ITestInterface
+        {
+        }
+    }
+}
