e0d3591793ffe5180c97e0f457a1320caa4d9aa1
zawisz <zawistowski.zawisz@gmail.com>
2016-05-23 17:36:52 +0200

    implemented basic operations in YeTiContainer

---
+++ src/Container/YeTiContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Container
+{
+    public class YeTiContainer
+    {
+        readonly Dictionary<Type, Type> _registrations = new Dictionary<Type, Type>(); 
+
+        public void Register<TRegistration, TImplementation>()
+        {
+            _registrations.Add(typeof(TRegistration), typeof(TImplementation));
+        }
+
+        public T Resolve<T>()
+        {
+            var requested_type = typeof (T);
+
+            Type actual_type = _registrations[requested_type];
+
+            var instance = Activator.CreateInstance(actual_type);
+
+            return (T) instance;
+        }
+    }
+}
